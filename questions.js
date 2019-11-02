f323dcc3d3db073e12282f710cf9dfed4ff4c068
austinev <bgotchall@gmail.com>
2019-11-02 18:36:43 -0500

    nearly done.  just could use a sort

--- questions.js
+++ questions.js
@@ -1,14 +1,27 @@
 var questions = [
-    {
-      title: "Primary colors include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    }
-    
-    ///etc.
-  ];
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "CSS stands for:",
+    choices: ["Colorful Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Cascading Style Sheets"],
+    answer: "Cascading Style Sheets"
+  },
+  {
+    title: "Where in an HTML document is the correct place to refer to an external style sheet?",
+    choices: ["In the <body> section", "In the <head> section", "At the end of the document", "anywhere"],
+    answer: "In the <head> section"
+  },
+  {
+    title: "Which HTML tag is used to define an internal style sheet?",
+    choices: ["<css>", "<style>", "<script>", "<header>"],
+    answer: "<style>"
+  }
+];
--- script.js
+++ script.js
@@ -11,26 +11,17 @@
 // Your application should also be responsive, ensuring that it adapts to multiple screen sizes.
 // Refer to the animated GIF below for a demonstration of the application functionality.
 
-var questions2 = [
-  {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
-  }
-];
+
 
 var high_scores_El = document.getElementById("hi_scores");
 var time_remaining_El = document.getElementById("time_remaining");
 var content_box_El = document.getElementById("boundary_box");
 var answer1_El = document.getElementById("answer1");
 
-var score= 75;
-var q=0;
+const game_length = 75;
+
+var score = game_length;
+var q = 0;
 var running = false; //if the timer is running decrement the score.
 var showing_result = 0;
 time_remaining_El.textContent = "Time: " + score;
@@ -45,9 +36,9 @@ function start_game() {
 function do_a_question() {
   $("#boundary_box").empty();
   var new_header = $("<h1></h1>");
-  new_header.text(questions2[q].title);
+  new_header.text(questions[q].title);
   new_header.addClass("question");
-  
+
   $("#boundary_box").append(new_header);
 
   for (i = 0; i < 4; i++) {
@@ -55,8 +46,8 @@ function do_a_question() {
     $(new_button).addClass(
       "btn btn-primary btn-sm button_show question_button"
     );
-    $(new_button).text(i + ": " + questions2[q].choices[i]);
-    $(new_button).attr("choice", questions2[q].choices[i]);
+    $(new_button).text(i + ": " + questions[q].choices[i]);
+    $(new_button).attr("choice", questions[q].choices[i]);
     $(new_button).on("click", check_answer);
     $("#boundary_box").append(new_button);
   }
@@ -73,6 +64,7 @@ interval = setInterval(function() {
     renderTime();
     if (score == 0) {
       running = false;
+      all_done();
     }
     if (showing_result > 0) {
       showing_result--;
@@ -81,7 +73,7 @@ interval = setInterval(function() {
         //get rid of the result after 2 ticks
         $("#result_id").delete;
         $("#hr").delete;
-        if (q == questions2.length) {
+        if (q == questions.length) {
           all_done();
         } else {
           do_a_question();
@@ -102,29 +94,28 @@ function all_done() {
   var new_header = $("<h1></h1>");
   new_header.text("All Done!");
 
-  //new_header.addClass("question");
-  //console.log("new header is", new_header);
   $("#boundary_box").append(new_header);
 
-  var my_string = "<p>Your final score is " + score + "</p>";
+  var my_string = "<p>Your final score is " + score + ".</p>";
   var new_p = $(my_string);
   console.log(my_string);
   $("#boundary_box").append(new_p);
-  my_string = "<p>Enter initials</p>";
-  var new_p = $(my_string);
-  $("#boundary_box").append(new_p);
 
-  var my_text_box = $(
-    '<input id="text_box"  type="text" name="Initials"  ></input>'
+  var new_form = $('<form class="form-inline initials_form"></form>');
+  var new_div = $(
+    '<div class="form-group mb-2"><label for="Initials_label" class="sr-only"></label><input type="text" readonly class="form-control-plaintext" id="Initials_label" value="Enter Initials"></div>'
   );
-  $("#boundary_box").append(my_text_box);
-
-  var new_button = $("<button>");
-  $(new_button).addClass("btn btn-primary btn-sm button_show question_button");
-  //$(new_button).addId("submit_button");
-  $(new_button).text("submit");
+  $(new_form).append(new_div);
+  new_div = $(
+    '<div class="form-group mx-sm-3 mb-2"><label for="Input_initials" class="sr-only">Password</label><input type="text" class="form-control-sm" id="text_box" ></div>'
+  );
+  $(new_form).append(new_div);
+  var new_button = $(
+    '<button type="text" class="btn btn-primary mb-2 question_button submit_button">Submit</button>'
+  );
   $(new_button).on("click", add_score_v2);
-  $("#boundary_box").append(new_button);
+  $(new_form).append(new_button);
+  $("#boundary_box").append(new_form);
 }
 
 function check_answer() {
@@ -132,8 +123,8 @@ function check_answer() {
     //if the answer is being shown, freeze out clicks
     var new_string = $(this).attr("choice");
     $("#boundary_box").append("<hr>");
-    if (questions2[q].answer === new_string) {
-      $("#boundary_box").append("<p class=\"result\">Correct!</p>");
+    if (questions[q].answer === new_string) {
+      $("#boundary_box").append('<p class="result">Correct!</p>');
     } else {
       $("#boundary_box").append('<p class="result">Incorrect!</p>');
       score = score - 15;
@@ -143,98 +134,105 @@ function check_answer() {
   } //if showing answer
 } //check_answer
 
-function add_score() {
-  var my_thing = document.getElementById("text_box");
-  console.log(my_thing.value); //I couldn't get the jquery one to work
-  // do local memory stuff and call and keep a list of high scores.
-  var thing=localStorage.getItem
- 
-  var my_array_of_scores=[];    
-  my_array_of_scores.push(  JSON.parse(localStorage.getItem("high_scores"))); //get the previous scores
-  console.log("the existing list:"+ my_array_of_scores);    
-  
-  my_array_of_scores.push(my_thing.value);  //add the new score
-  my_array_of_scores.push(score);
-  console.log("the list with new things added:" +my_array_of_scores);
-  my_array_of_scores=sort_array(my_array_of_scores);    //sort them highest to lowest
-  //store the big array
-  localStorage.setItem("high_scores", JSON.stringify(my_array_of_scores));
-  draw_score_page();
-}
-
 function add_score_v2() {
   //this one is goign to avoid the stringify thing and just do ordered pairs
-  
+
   var my_thing = document.getElementById("text_box");
   console.log(my_thing.value); //I couldn't get the jquery one to work
-  localStorage.setItem(my_thing.value,score);
+  localStorage.setItem(my_thing.value, score);
 
   draw_score_page();
 }
 
-function draw_score_page(){
+function draw_score_page() {
   //draw the scores, add a clear button and a new game button
   //first grab an array of the existing, old scores:
-  var my_array_of_scores=[];
-  for (var i=0;i<localStorage.length;i++){
-    var temp_thing=localStorage.key(i);
-    temp_thing=localStorage.getItem(localStorage.key(i));
-    my_array_of_scores.push(localStorage.key(i));  //push appends to the array.
+  var my_array_of_scores = [];
+  for (var i = 0; i < localStorage.length; i++) {
+    var temp_thing = localStorage.key(i);
+    temp_thing = localStorage.getItem(localStorage.key(i));
+    my_array_of_scores.push(localStorage.key(i)); //push appends to the array.
     my_array_of_scores.push(localStorage.getItem(localStorage.key(i)));
   }
 
   $("#boundary_box").empty();
-  
-  //build up the table.  
-  var my_table=$("<table></table>");  // table, then <tr> then <td>
 
-  for (i=0;i<my_array_of_scores.length;i=i+2){
-    var my_row=$("<tr></tr>");
-    var my_data=$("<td id=\"hi_scores_table\"></td>");
-    $(my_data).text((i/2)+1+": "+my_array_of_scores[i]+"- "+my_array_of_scores[i+1]);
+  //build up the table.
+  var my_table = $("<table></table>"); // table, then <tr> then <td>
+
+  for (i = 0; i < my_array_of_scores.length; i = i + 2) {
+    var my_row = $("<tr></tr>");
+    var my_data = $('<td id="hi_scores_table"></td>');
+    $(my_data).text(
+      i / 2 +
+        1 +
+        ": " +
+        my_array_of_scores[i] +
+        "- " +
+        my_array_of_scores[i + 1]
+    );
     my_row.append(my_data);
     my_table.append(my_row);
   }
   //$(my_table).addId("hi_scores")
-  $("#boundary_box").append(my_table);    //table is built, just add it now
+  $("#boundary_box").append(my_table); //table is built, just add it now
 
-  new_thing = $('<button type="button" id="start_button" class="btn btn-primary btn-sm">Clear High Scores</button>' );
-  new_thing.on("click", clear_high_scores);
-  $("#boundary_box").append(new_thing);
-  new_thing = $('<button type="button" id="start_button" class="btn btn-primary btn-sm">Go Back</button>' );
+  new_thing = $(
+    '<button type="button" id="score_button" class="btn btn-primary btn-sm">Go Back</button>'
+  );
   new_thing.on("click", initialize_game);
   $("#boundary_box").append(new_thing);
-
+  new_thing = $(
+    '<button type="button" id="score_button" class="btn btn-primary btn-sm">Clear High Scores</button>'
+  );
+  new_thing.on("click", clear_high_scores);
+  $("#boundary_box").append(new_thing);
 }
 
 function initialize_game() {
   //draw the instructions and the start button.  done at the very start and after entering initials.
-  q=0;
-  score=75;
+  q = 0;
+  score = game_length;
   $("#boundary_box").empty();
   renderTime();
-  
+
   var new_thing = $("<h1>Coding Quiz Challenge</h1>");
   $("#boundary_box").append(new_thing);
-  new_thing = $( "<p>Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score Time by ten seconds!</p>");
+  new_thing = $(
+    "<p>Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score Time by ten seconds!</p>"
+  );
   $("#boundary_box").append(new_thing);
-  new_thing = $('<button type="button" id="start_button" class="btn btn-primary btn-sm">Start Quiz</button>' );
+  new_thing = $(
+    '<button type="button" id="start_button" class="btn btn-primary btn-sm">Start Quiz</button>'
+  );
   new_thing.on("click", start_game);
   $("#boundary_box").append(new_thing);
 }
 
-function sort_array(my_array){
-//take an array wich is a list of initials,score pairs and sort them from highest to lowest, return that sorted array.
+function sort_array(my_array) {
+  //take an array wich is a list of initials,score pairs and sort them from highest to lowest, return that sorted array.
   // var pair
   // for (var i=0;i<my_array.length-1;i=i+2){
 
-
   // }
 
-  return my_array;      //for now just return the unsorted list
+  return my_array; //for now just return the unsorted list
 }
 
-function clear_high_scores(){
+function clear_high_scores() {
   localStorage.clear();
   draw_score_page();
 }
+
+//form prototype:
+// <form class="form-inline">
+//   <div class="form-group mb-2">
+//     <label for="Initials_label" class="sr-only"></label>
+//     <input type="text" readonly class="form-control-plaintext" id="Initials_label" value="Enter Initials">
+//   </div>
+//   <div class="form-group mx-sm-3 mb-2">
+//     <label for="Input_initials" class="sr-only">Password</label>
+//     <input type="text" class="form-control" id="Submit" >
+//   </div>
+//   <button type="submit" class="btn btn-primary mb-2">Submit</button>
+// </form>
