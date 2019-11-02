b92461cd90965975f960b82a053a3818d09537b5
austinev <bgotchall@gmail.com>
2019-11-02 16:24:31 -0500

    really close now.  just some final details and styling needed

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 // From scratch, build a timer-based quiz application that stores high scores client-side. Following the common templates for user stories, we can frame this challenge as follows:
-
 // As a coding bootcamp student
 // I want to take a timed quiz on JavaScript fundamentals that stores high scores
 // so that I can gauge my progress compared to my peers
 // How do you deliver this? Here are some guidelines:
-
 // Play proceeds as follows:
-
 // The user arrives at the landing page and is presented with a call-to-action to "Start Quiz." Also note the navigation option to "View Highscores" and the "Time" value set at 0.
-
 // Clicking the "Start Quiz" button presents the user with a series of questions. The timer is initialized with a value and immediately begins countdown.
-
 // Score is calculated by time remaining. Answering quickly and correctly results in a higher score. Answering incorrectly results in a time penalty (for example, 15 seconds are subtracted from time remaining).
-
 // When time runs out and/or all questions are answered, the user is presented with their final score and asked to enter their initials. Their final score and initials are then stored in localStorage.
-
 // Your application should also be responsive, ensuring that it adapts to multiple screen sizes.
-
 // Refer to the animated GIF below for a demonstration of the application functionality.
 
 var questions2 = [
@@ -29,11 +21,6 @@ var questions2 = [
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses"
-  },
-  {
-    title: "The shortest Month",
-    choices: ["Febrary", "March", "July", "December"],
-    answer: "Febrary"
   }
 ];
 
@@ -44,16 +31,9 @@ var answer1_El = document.getElementById("answer1");
 
 var score= 75;
 var q=0;
-var running = false; //if the timer is running
+var running = false; //if the timer is running decrement the score.
 var showing_result = 0;
 time_remaining_El.textContent = "Time: " + score;
-
-// $("#start_button").on("click", function() {
-//   //start button just kicks off the timer and paints the first question since it
-//   //is the first one without a previous answer.
-//   running = true;
-//   do_a_question();
-// }); //start button
 
 initialize_game(); //draw the start page
 
@@ -67,7 +47,7 @@ function do_a_question() {
   var new_header = $("<h1></h1>");
   new_header.text(questions2[q].title);
   new_header.addClass("question");
-  console.log("new header is", new_header);
+  
   $("#boundary_box").append(new_header);
 
   for (i = 0; i < 4; i++) {
@@ -143,7 +123,7 @@ function all_done() {
   $(new_button).addClass("btn btn-primary btn-sm button_show question_button");
   //$(new_button).addId("submit_button");
   $(new_button).text("submit");
-  $(new_button).on("click", add_score);
+  $(new_button).on("click", add_score_v2);
   $("#boundary_box").append(new_button);
 }
 
@@ -152,16 +132,10 @@ function check_answer() {
     //if the answer is being shown, freeze out clicks
     var new_string = $(this).attr("choice");
     $("#boundary_box").append("<hr>");
-    console.log(questions2[q].answer);
-    console.log(new_string);
-    console.log(q);
-
     if (questions2[q].answer === new_string) {
-      console.log("correct");
-      $("#boundary_box").append("<p >Correct</p>");
+      $("#boundary_box").append("<p class=\"result\">Correct!</p>");
     } else {
-      console.log("wrong");
-      $("#boundary_box").append('<p id="result_id">Incorrect, sorry</p>');
+      $("#boundary_box").append('<p class="result">Incorrect!</p>');
       score = score - 15;
     }
     q++;
@@ -188,20 +162,42 @@ function add_score() {
   draw_score_page();
 }
 
+function add_score_v2() {
+  //this one is goign to avoid the stringify thing and just do ordered pairs
+  
+  var my_thing = document.getElementById("text_box");
+  console.log(my_thing.value); //I couldn't get the jquery one to work
+  localStorage.setItem(my_thing.value,score);
+
+  draw_score_page();
+}
+
 function draw_score_page(){
   //draw the scores, add a clear button and a new game button
+  //first grab an array of the existing, old scores:
   var my_array_of_scores=[];
-  my_array_of_scores.push(  JSON.parse(localStorage.getItem("high_scores"))); //get the previous scores
-  $("#boundary_box").empty();
+  for (var i=0;i<localStorage.length;i++){
+    var temp_thing=localStorage.key(i);
+    temp_thing=localStorage.getItem(localStorage.key(i));
+    my_array_of_scores.push(localStorage.key(i));  //push appends to the array.
+    my_array_of_scores.push(localStorage.getItem(localStorage.key(i)));
+  }
 
-  console.log("in the draw page functin, here is the array I am working on:"+my_array_of_scores);
-  //do one score to test:
-  var new_thing=$("<p></p>");
-  new_thing.text(my_array_of_scores[0]);
-  $("#boundary_box").append(new_thing);
-  new_thing=$("<p></p>");
-  new_thing.text(my_array_of_scores[1]);
-  $("#boundary_box").append(new_thing);
+  $("#boundary_box").empty();
+  
+  //build up the table.  
+  var my_table=$("<table></table>");  // table, then <tr> then <td>
+
+  for (i=0;i<my_array_of_scores.length;i=i+2){
+    var my_row=$("<tr></tr>");
+    var my_data=$("<td id=\"hi_scores_table\"></td>");
+    $(my_data).text((i/2)+1+": "+my_array_of_scores[i]+"- "+my_array_of_scores[i+1]);
+    my_row.append(my_data);
+    my_table.append(my_row);
+  }
+  //$(my_table).addId("hi_scores")
+  $("#boundary_box").append(my_table);    //table is built, just add it now
+
   new_thing = $('<button type="button" id="start_button" class="btn btn-primary btn-sm">Clear High Scores</button>' );
   new_thing.on("click", clear_high_scores);
   $("#boundary_box").append(new_thing);
@@ -212,14 +208,12 @@ function draw_score_page(){
 }
 
 function initialize_game() {
-  //draw the instructions and the start button:
-  //alert("initializing game.  at the very start and after entering initials");
+  //draw the instructions and the start button.  done at the very start and after entering initials.
   q=0;
   score=75;
   $("#boundary_box").empty();
   renderTime();
   
-
   var new_thing = $("<h1>Coding Quiz Challenge</h1>");
   $("#boundary_box").append(new_thing);
   new_thing = $( "<p>Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score Time by ten seconds!</p>");
@@ -227,9 +221,7 @@ function initialize_game() {
   new_thing = $('<button type="button" id="start_button" class="btn btn-primary btn-sm">Start Quiz</button>' );
   new_thing.on("click", start_game);
   $("#boundary_box").append(new_thing);
-  //localStorage.clear();
 }
-
 
 function sort_array(my_array){
 //take an array wich is a list of initials,score pairs and sort them from highest to lowest, return that sorted array.
@@ -243,7 +235,6 @@ function sort_array(my_array){
 }
 
 function clear_high_scores(){
-localStorage.clear();
-draw_score_page();
-
+  localStorage.clear();
+  draw_score_page();
 }
